b10700f42fac1641211184048723b5acf5adfce5
Prasad Dalavi <prasad01dalavi@gmail.com>
2021-03-23 16:05:54 +0530

    Update concept.py

--- Logging/concept.py
+++ Logging/concept.py
@@ -19,3 +19,20 @@ INFO: Line:15 2019-02-26 16:47:16,927 ==> This info looks cool!
 INFO: Line:15 2019-02-26 16:47:17,338 ==> This info looks cool!
 INFO: Line:15 2019-02-26 16:47:17,508 ==> This info looks cool!
 """
+
+# For Log File and Console Output Setup
+"""
+logging.basicConfig(level=LOG_LEVEL_VALUE[LOG_LEVEL],
+                    filename=log_file,
+                    filemode='a',
+                    format='[%(levelname)s] %(asctime)s %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S',)
+
+# Set up logging to console
+console = logging.StreamHandler()
+console.setLevel(LOG_LEVEL_VALUE[LOG_LEVEL])
+# Set a format which is simpler for console use
+formatter = logging.Formatter('[%(levelname)s] %(asctime)s %(message)s')
+console.setFormatter(formatter)
+logging.getLogger("").addHandler(console)
+"""
